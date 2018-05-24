c2cb455ecb64cb7c32ef91a62877104093ee36ea
Chuck Collins <chuck.collins@gmail.com>
2018-05-24 18:45:02 -0500

    vscode install

--- vscode/install.sh
+++ vscode/install.sh
@@ -12,6 +12,47 @@ if test "$(which code)"; then
 
 	# from `code --list-extensions`
 	modules="
+CraigMaslowski.erb
+Equinusocio.vsc-material-theme
+James-Yu.latex-workshop
+PeterJausovec.vscode-docker
+QassimFarid.ejs-language-support
+aaronthomas.vscode-snazzy-operator
+adamvoss.yaml
+arcticicestudio.nord-visual-studio-code
+castwide.solargraph
+codezombiech.gitignore
+cometeer.spacemacs
+dbaeumer.vscode-eslint
+eamodio.gitlens
+ecmel.vscode-html-css
+formulahendry.auto-close-tag
+gerane.Theme-Blackboard
+gerane.Theme-PastelsonDark
+humao.rest-client
+idleberg.hopscotch
+jansenfuller.crayon-vscode
+kalitaalexey.vscode-rust
+kumar-harsh.graphql-for-vscode
+mauve.terraform
+ms-python.python
+ms-vscode.atom-keybindings
+ms-vscode.cpptools
+ms-vscode.Go
+raashida.fixthecode-vs
+rebornix.Ruby
+redhat.java
+robertohuertasm.vscode-icons
+robinbentley.sass-indented
+saviorisdead.RustyCode
+sbrink.elm
+thenikso.github-plus-theme
+vsciot-vscode.vscode-arduino
+vscjava.vscode-java-debug
+vscjava.vscode-java-pack
+vscjava.vscode-java-test
+vscjava.vscode-maven
+zhuangtongfa.Material-theme
 "
 	for module in $modules; do
 		code --install-extension "$module" || true
